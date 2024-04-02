21e8a835a936878c4cec7105d08540b9a34689f9
Bilal Durmuş <bilaldurmus08@gmail.com>
2024-04-02 11:13:18 +0300

    Some arrangments

--- lib/gradiant_container.dart
+++ lib/gradiant_container.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 
 const startAlingment = Alignment.topLeft;
@@ -5,13 +6,9 @@ const endAlignment = Alignment.bottomRight;
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key, required this.colors});
-  GradientContainer.purple({super.key})
+   GradientContainer.blue({super.key})
       : colors = [Colors.blue.shade200, Colors.blueGrey.shade500];
   final List<Color> colors;
-
-  void rollDice() {
-    //.....
-  }
 
   @override
   Widget build(context) {
@@ -24,31 +21,7 @@ class GradientContainer extends StatelessWidget {
         ),
       ),
       child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-6.png',
-              width: 200,
-            ),
-            const SizedBox(
-              height: 25,
-            ),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                /*padding: const EdgeInsets.only(
-                  top: 20
-                  ),*/
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(
-                  fontSize: 28,
-                ),
-              ),
-              child: const Text('Roll a Dice'),
-            ),
-          ],
-        ),
+        child: DiceRoller(),
       ),
     );
   }
